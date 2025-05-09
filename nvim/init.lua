6cb8b8d9bf076dc1f87837a5c8d80ef470b17d77
dmitryvasilkov <dmitryvasilkovw@gmail.com>
2025-05-09 04:24:50 +0300

    update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,4 @@
+-- Пакетный менеджер lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -8,38 +9,94 @@ if not vim.loop.fs_stat(lazypath) then
     lazypath,
   })
 end
+
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+  -- Файловый менеджер в виде дерева
+  "nvim-tree/nvim-tree.lua",
 
-  -- Git
-  "tpope/vim-fugitive",
-  "lewis6991/gitsigns.nvim",
+  -- Иконки файлов (для nvim-tree, lualine и других)
+  "nvim-tree/nvim-web-devicons",
 
-  -- Поиск и файлы
-  { "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
+  -- Fuzzy-поиск файлов, строк и др.
+  "nvim-telescope/telescope.nvim",
 
-  -- LSP и автодополнение
-  "neovim/nvim-lspconfig",
-  "hrsh7th/nvim-cmp",
-  "hrsh7th/cmp-nvim-lsp",
-  "L3MON4D3/LuaSnip",
+  -- Вспомогательная библиотека (обязательна для Telescope и других)
+  "nvim-lua/plenary.nvim",
 
-  -- Статусбар
+  -- Продвинутая подсветка синтаксиса и парсинг кода
+  "nvim-treesitter/nvim-treesitter",
+
+  -- Статусбар внизу
   "nvim-lualine/lualine.nvim",
 
-  -- Цветовая тема
+  -- TODO-комментарии с подсветкой и поиском
+  "folke/todo-comments.nvim",
+
+  -- Работа с Git (аналог `git` внутри Neovim)
+  "tpope/vim-fugitive",
+
+  -- Отображение изменений в гите на полях (git diff)
+  "lewis6991/gitsigns.nvim",
+
+  -- Конфигурации LSP серверов (например, для Python, Lua и т.п.)
+  "neovim/nvim-lspconfig",
+
+  -- Ядро системы автодополнения
+  "hrsh7th/nvim-cmp",
+
+  -- Источник автодополнения от LSP
+  "hrsh7th/cmp-nvim-lsp",
+
+  -- Источник автодополнения из открытых буферов
+  "hrsh7th/cmp-buffer",
+
+  -- Источник автодополнения путей к файлам
+  "hrsh7th/cmp-path",
+
+  -- Движок сниппетов (шаблонов кода)
+  "L3MON4D3/LuaSnip",
+
+  -- Поддержка сниппетов в системе автодополнения
+  "saadparwaiz1/cmp_luasnip",
+
+  -- Автоматическое закрытие скобок, кавычек и т.п.
+  "windwp/nvim-autopairs",
+
+  -- Комментирование кода (`gc` + движение)
+  "numToStr/Comment.nvim",
+
+  -- UI-обёртка над LSP: подсказки, рефакторинг, подсветка и др.
+  "glepnir/lspsaga.nvim",
+
+  -- Тема Gruvbox (одна из двух подключённых)
   "ellisonleao/gruvbox.nvim",
+
+  -- Тема Gruvbox с приоритетом и автоустановкой
+  {
+    "ellisonleao/gruvbox.nvim",
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme("gruvbox")
+    end,
+  },
 })
 
--- Основные настройки
+config = function()
+  require("gruvbox").setup({
+    contrast = "hard",
+  })
+  vim.cmd.colorscheme("gruvbox")
+end
+
+--  Основные настройки
 vim.o.number = true
-vim.o.relativenumber = fasle
+vim.o.relativenumber = false
 vim.o.termguicolors = true
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
-vim.o.expandtab = truge
+vim.o.expandtab = true
 vim.cmd.colorscheme("gruvbox")
 
 -- Treesitter
@@ -47,7 +104,7 @@ require("nvim-treesitter.configs").setup({
   highlight = { enable = true },
 })
 
--- LSP
+--  LSP
 local lspconfig = require("lspconfig")
 lspconfig.lua_ls.setup({})
 lspconfig.pyright.setup({})
