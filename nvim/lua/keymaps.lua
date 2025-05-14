dfce8c60c07e51488f4808e22fbed59a8d35d48f
dmitryvasilkov <dmitryvasilkovw@gmail.com>
2025-05-14 21:50:41 +0300

    update nvim config

---
+++ nvim/lua/keymaps.lua
@@ -0,0 +1,2 @@
+vim.keymap.set("n", "<C-p>", ":Telescope find_files<CR>", { noremap = true })
+vim.keymap.set("n", "<C-f>", ":Telescope live_grep<CR>", { noremap = true })
---
+++ nvim/lua/lsp.lua
@@ -0,0 +1,6 @@
+local lspconfig = require("lspconfig")
+
+lspconfig.lua_ls.setup({})
+lspconfig.pyright.setup({})
+lspconfig.tsserver = nil
+lspconfig.ts_ls.setup({})
